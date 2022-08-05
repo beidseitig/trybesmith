50a0e700a829aeb2b1bed76f4f9da4b7fc210c6e
Ramos <beidseitig@gmail.com>
2022-08-05 18:40:54 -0300

    fix: git test requirement for Secret

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -6,7 +6,7 @@ const jwtDuration: SignOptions = { expiresIn: '7d' };
 // https://stackoverflow.com/questions/54496398/typescript-type-string-undefined-is-not-assignable-to-type-string
 // const secretPassword: Secret = process.env.SECRET!;
 
-const secretPassword: Secret = process.env.SECRET || '';
+const secretPassword: Secret = process.env.SECRET || 'testCheck';
 
 export default function tokenGeneration(username: string): string {
   const token = jwt.sign({ username }, secretPassword, jwtDuration);
